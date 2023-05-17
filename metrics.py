4465c9c7473773d3c677cf41db1273e2ffd816f8
phuong <phuongtd1107@gmail.com>
2023-05-17 16:40:55 +0700

    add def compute_mean

--- metrics.py
+++ metrics.py
@@ -5,5 +5,10 @@ def compute_rmse(y_pred,y):
     return rmse
 
 #TODO
-def compute_mean():
-    pass
+def compute_mean(list_1):
+    a=len(list_1)
+    sum_list_1 = 0
+    for i in list_1:
+        sum_list_1+=i
+    return sum_list_1/a
+compute_mean()
